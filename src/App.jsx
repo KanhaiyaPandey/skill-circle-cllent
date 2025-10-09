55288796765deb59d3c0958488906ca480b8a7a0
KanhaiyaPandey <kanhaiyapandey2232@gmail.com>
2025-10-09 17:31:53 +0530

    new background

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
     <div className="poppins">
 
       <div className=" relative flex items-center justify-center h-screen">
-        <img src="/new_ai.jpg" className=" w-full h-full object-cover absolute -z-10 top-0 left-0" alt="" />
+        <img src="/circle_logo.jpg" className=" w-full h-full object-cover absolute -z-10 top-0 left-0" alt="" />
         <LandingPage />
       </div>
 
